8f6b7a2223113c19bab80b94b224ee77fa87d5ae
Matthew Lofgren <matthewlofgren@proton.me>
2024-02-08 22:11:52 -0500

    add default lat lon

--- Frontend/unhoused-outreach-ui/src/features/navbar/navbar.tsx
+++ Frontend/unhoused-outreach-ui/src/features/navbar/navbar.tsx
@@ -4,13 +4,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import './navbar.css';
 
 function UnhousedOutreachNavbar() {
+  // TODO: Get these from outreach team data.
+  const defaultLatitude =  38.96577;
+  const defaultLongitude = -77.35503;
+  const mapRouteLink = `map/${defaultLatitude}/${defaultLongitude}`;
+
   return (
     <Navbar className="navbar">
       <Container>
         <Navbar.Brand className="navbar-main navbar-link" href="/">Unhoused Outreach</Navbar.Brand>
           <Nav className="navbar-items">
             <Nav.Link className="navbar-link" href="/">Neighbors</Nav.Link>
-            <Nav.Link className="navbar-link" href="map">Map</Nav.Link>
+            <Nav.Link className="navbar-link" href={mapRouteLink}>Map</Nav.Link>
           </Nav>
       </Container>
     </Navbar>
